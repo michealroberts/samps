7776a60a6eb0f6141ea92a0a577233eb6e5d4a49
michealroberts <michael@observerly.com>
2025-05-06 12:46:13 +0100

    feat: add SerialWriteError to errors in samps module
    feat: add SerialWriteError to errors in samps module

--- test/test_errors.py
+++ test/test_errors.py
@@ -7,7 +7,7 @@
 
 import unittest
 
-from samps import SerialReadError
+from samps import SerialReadError, SerialWriteError
 
 # **************************************************************************************
 
@@ -31,6 +31,31 @@ class TestSerialReadErrorError(unittest.TestCase):
         test_message = "An error occurred."
         with self.assertRaises(SerialReadError) as context:
             raise SerialReadError(test_message)
+        self.assertEqual(str(context.exception), test_message)
+
+
+# **************************************************************************************
+
+
+class TestSerialWriteError(unittest.TestCase):
+    def test_inheritance(self):
+        """Test that SerialWriteError inherits from the built-in Exception class."""
+        self.assertTrue(issubclass(SerialWriteError, Exception))
+
+    def test_error_message(self):
+        """Test that the error message is correctly set and retrieved."""
+        test_message = "This is a test error message."
+        error = SerialWriteError(test_message)
+        self.assertEqual(str(error), test_message)
+
+    def test_raising_error(self):
+        """
+        Test that raising SerialWriteError with a specific message
+        results in that message being available on the exception.
+        """
+        test_message = "An error occurred."
+        with self.assertRaises(SerialWriteError) as context:
+            raise SerialWriteError(test_message)
         self.assertEqual(str(context.exception), test_message)
 
 
