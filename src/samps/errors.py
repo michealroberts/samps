c6fd5893dd2645972f4796f957b3e1dc85166d31
michealroberts <michael@observerly.com>
2025-05-06 12:15:09 +0100

    feat: add SerialReadError to errors in samps module
    feat: add SerialReadError to errors in samps module

---
+++ src/samps/errors.py
@@ -0,0 +1,18 @@
+# **************************************************************************************
+
+# @package        samps
+# @license        MIT License Copyright (c) 2025 Michael J. Roberts
+
+# **************************************************************************************
+
+
+class SerialReadError(Exception):
+    """
+    Exception class for serial read errors.
+    """
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+# **************************************************************************************
---
+++ test/test_errors.py
@@ -0,0 +1,37 @@
+# **************************************************************************************
+
+# @package        samps
+# @license        MIT License Copyright (c) 2025 Michael J. Roberts
+
+# **************************************************************************************
+
+import unittest
+
+from samps import SerialReadError
+
+# **************************************************************************************
+
+
+class TestSerialReadErrorError(unittest.TestCase):
+    def test_inheritance(self):
+        """Test that SerialReadError inherits from the built-in Exception class."""
+        self.assertTrue(issubclass(SerialReadError, Exception))
+
+    def test_error_message(self):
+        """Test that the error message is correctly set and retrieved."""
+        test_message = "This is a test error message."
+        error = SerialReadError(test_message)
+        self.assertEqual(str(error), test_message)
+
+    def test_raising_error(self):
+        """
+        Test that raising SerialReadError with a specific message
+        results in that message being available on the exception.
+        """
+        test_message = "An error occurred."
+        with self.assertRaises(SerialReadError) as context:
+            raise SerialReadError(test_message)
+        self.assertEqual(str(context.exception), test_message)
+
+
+# **************************************************************************************
